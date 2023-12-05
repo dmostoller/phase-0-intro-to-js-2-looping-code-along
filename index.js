71c9f5acf71c568367c38c8088bdbf9665f13bf2
David Mostoller <dmostoller@gmail.com>
2023-12-04 19:12:21 -0500

    completed

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+
+var nameArray = []
+var messageArray = []
+
+function writeCards(nameArray, holiday) {
+    for ( let i = 0; i < nameArray.length; i++) {
+        messageArray.push("Thank you, " + nameArray[i] + ", for the wonderful " + holiday + " gift!")
+    }
+    return messageArray;
+}
+
+writeCards(nameArray, holiday);
+
+
+function countDown(int) {
+    let count = int
+    while (count >= 0) {
+        console.log(count--)
+    }
+}
+
+countDown(int)
